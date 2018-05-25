e42ec6ea48d3f0dacde1b6fb2445ed48610fb3bf
carlganz <carlganz@gmail.com>
2018-05-24 20:08:41 -0700

    Support SE and cv

--- src/main/scala/com/orangutanalytics/scurvy/surveyStat.scala
+++ src/main/scala/com/orangutanalytics/scurvy/surveyStat.scala
@@ -1,8 +1,21 @@
 package com.orangutanalytics.scurvy
 
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.Column
 
 case class SurveyStat (
   estimate: DataFrame,
-  variance: DataFrame
-)
+  variance: DataFrame,
+  statistic: Column,
+  variable: Column
+) {
+  def SE : DataFrame = variance.withColumn("SE", sqrt("variance")).drop("variance")
+  def cv : DataFrame = {
+    estimate.withColumn(".temp_id", monotonically_increasing_id()).join(
+      SE.withColumn(".temp_id", monotonically_increasing_id()), ".temp_id"
+    ).drop(".temp_id").withColumn("cv", col("SE")/statistic).drop("SE", statistic.toString())
+  }
+  /*deff : DataFrame
+  confint(method : String) : Dataframe*/
+}
