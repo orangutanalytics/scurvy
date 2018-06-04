6e3b8983f97728982b3ad9f5efe2a16b270e6120
carlganz <carlganz@gmail.com>
2018-06-04 13:08:12 -0700

    refactor using more fp approach

--- src/main/scala/com/orangutanalytics/scurvy/surveyDesign.scala
+++ src/main/scala/com/orangutanalytics/scurvy/surveyDesign.scala
@@ -14,47 +14,13 @@ sealed trait SurveyDesign {
     case true => df.agg(sum(pweight)).head().getDouble(0)
     case false => df.count().toDouble  
   }
-  def svyTotal(est: Column) : SurveyStat
-  def svyMean(est: Column) : SurveyStat
-  //def svyQuantile(est: Column*, quantile: Double = 0.5) : SurveyStat
-  def svyRatio(numerator: Column, denominator: Column) : SurveyStat
-  //def svyFreq(est: Column*) : SurveyStat
-  
-  // models
-  //def svyGlm() : SurveyModelStat
-
   // transformations
   def svySubset(bool: Column) : SurveyDesign
-  //def svyGroupBy(by: Column*) : GroupedSurveyDesign
-  //def svyCalibrate() : SurveyDesign
-  //def svyPostStratify() : SurveyDesign
-  //def svyRake() : SurveyDesign
-  def svyFilter(est: Column) : DataFrame = {
+  def svyFilterMissing(est: Column) : DataFrame = {
     df.withColumn(pweight.toString(), when(est.isNotNull, pweight).otherwise(0)).withColumn(est.toString(), when(est.isNotNull, est).otherwise(0))
   }
 
 }
-
-/*sealed trait GroupedSurveyDesign extends SurveyDesign {
-  def df: DataFrame
-  def group: Array[Column]
-  def pweight: Column
-  def svyTotal(est: Column) : SurveyStat = new SurveyStat(
-    estimate = {
-      df.groupBy(group.filter(_ != None).map(x => x): _*).agg((sum(est * pweight)).alias("total"))
-    },
-    variance = {
-      df.groupBy(group.filter(_ != None).map(x => x): _*).agg((sum(est * pweight)).alias("total"))
-    }
-  )
-  def svySubset(bool: Column) : GroupedSurveyDesign
-  
-  // weighted mean of continuous variable
-  override def svyMean(est: Column) : SurveyStat = new SurveyStat(
-    estimate = df.groupBy(group.filter(_ != None).map(x => x): _*).agg((sum(est * pweight)/sum(pweight)).alias("mean")),
-    variance = df.groupBy(group.filter(_ != None).map(x => x): _*).agg((sum(est * pweight)/sum(pweight)).alias("mean"))
-  )
-}*/
 
 case class TsDesign (
   df: DataFrame,
@@ -69,104 +35,15 @@ case class TsDesign (
     case (None, Some(strata)) => df.count().toInt - df.select(strata).distinct().count().toInt
     case (Some(cluster), Some(strata)) => df.groupBy(strata).agg(countDistinct(cluster).alias("total")).agg(sum("total")).head().getLong(0).toInt - df.select(strata).distinct().count().toInt
     }
-  
-  override def svyTotal(est: Column) : SurveyStat = new SurveyStat(
-    estimate = svyFilter(est).agg(sum(est * pweight).alias("total")),
-    variance = (cluster, strata) match {
-    case (None, None) => svyFilter(est).agg((count(est) * (1 - fpc) * var_samp(est * pweight)).alias("variance"))
-    case (Some(cluster), None) => svyFilter(est).groupBy(cluster).agg(sum(est * pweight).alias("total"))
-        .agg((count("total") * (1 - fpc) * var_samp("total")).alias("variance"))
-    case (None, Some(strata)) =>  svyFilter(est).groupBy(strata).agg((count(est) * (1 - fpc) * var_samp(est * pweight)).alias("variance"))
-        .agg(sum("variance").alias("variance"))
-    case (Some(cluster), Some(strata)) => svyFilter(est).groupBy(strata, cluster).agg(sum(est * pweight).alias("total")).
-        groupBy(strata).agg((count("total") * (1 - fpc) * var_samp("total")).alias("vari")).agg(sum("vari").alias("variance"))
-    },
-  statistic = col("total")
-  )
-  override def svyMean(est: Column) : SurveyStat = new SurveyStat(
-    estimate = svyFilter(est).agg((sum(est * pweight)/sum(pweight)).alias("mean")),
-    variance = (cluster, strata) match {
-    case (None, None) => svyFilter(est).
-        agg((sum(pweight * (est - (svyFilter(est).agg((sum(est * pweight)/sum(pweight)).alias("estimate"))
-          .select("estimate").head().getDouble(0))))/(svyFilter(est).agg(sum(pweight).alias("total")).head().getDouble(0))).alias("mean")).
-        agg((count("mean") * (1 - fpc) * var_samp("mean")).alias("variance"))
-    case (Some(cluster), None) => svyFilter(est).
-        groupBy(cluster).agg((sum(pweight * (est - (svyFilter(est).agg((sum(est * pweight)/sum(pweight)).alias("estimate"))
-          .select("estimate").head().getDouble(0))))/(svyFilter(est).agg(sum(pweight).alias("total")).head().getDouble(0))).alias("mean")).
-        agg((count("mean") * (1 - fpc) * var_samp("mean")).alias("variance"))
-    case (None, Some(strata)) => svyFilter(est).
-        groupBy(strata).agg((sum(pweight * (est - (svyFilter(est).agg((sum(est * pweight)/sum(pweight)).alias("estimate"))
-          .select("estimate").head().getDouble(0))))/(svyFilter(est).agg(sum(pweight).alias("total")).head().getDouble(0))).alias("mean")).
-        agg((count("mean") * (1 - fpc) * var_samp("mean")).alias("variance"))
-    case (Some(cluster), Some(strata)) => svyFilter(est).
-        groupBy(strata, cluster).agg((sum(pweight * (est - (svyFilter(est).agg((sum(est * pweight)/sum(pweight)).alias("estimate"))
-          .select("estimate").head().getDouble(0))))/(svyFilter(est).agg(sum(pweight).alias("total")).head().getDouble(0))).alias("mean")).
-        groupBy(strata).agg((count("mean") * (1 - fpc) * var_samp("mean")).alias("vari")).agg(sum("vari").alias("variance"))
-    },
-    statistic = col("mean")
-  )
-  override def svyRatio(numerator: Column, denominator: Column) : SurveyStat = new SurveyStat(
-    estimate = df.withColumn(pweight.toString(), when(numerator.isNotNull && denominator.isNotNull, pweight).otherwise(0)).
-    withColumn(numerator.toString(), when(numerator.isNotNull, numerator).otherwise(0)).
-    withColumn(denominator.toString(), when(denominator.isNotNull, denominator).otherwise(0)).
-    agg((sum(numerator * pweight)/sum(denominator * pweight)).alias("ratio")),
-    variance = (cluster, strata) match {
-    case (None, None) => df.withColumn(pweight.toString(), when(numerator.isNotNull && denominator.isNotNull, pweight).otherwise(0)).
-    withColumn(numerator.toString(), when(numerator.isNotNull, numerator).otherwise(0)).
-    withColumn(denominator.toString(), when(denominator.isNotNull, denominator).otherwise(0)).agg(var_samp(numerator * pweight)/sum(pweight).alias("variance"))
-    case (Some(cluster), None) => df.withColumn(pweight.toString(), when(numerator.isNotNull && denominator.isNotNull, pweight).otherwise(0)).
-    withColumn(numerator.toString(), when(numerator.isNotNull, numerator).otherwise(0)).
-    withColumn(denominator.toString(), when(denominator.isNotNull, denominator).otherwise(0)).
-        groupBy(cluster).agg((sum(pweight * (numerator - (df.agg((sum(numerator * pweight)/sum(pweight)).alias("estimate"))
-          .select("estimate").head().getDouble(0))))/(df.agg(sum(pweight).alias("total")).head().getDouble(0))).alias("mean")).
-        agg((count("mean") * (1 - fpc) * var_samp("mean")).alias("variance"))
-    case (None, Some(strata)) => df.withColumn(pweight.toString(), when(numerator.isNotNull && denominator.isNotNull, pweight).otherwise(0)).
-    withColumn(numerator.toString(), when(numerator.isNotNull, numerator).otherwise(0)).
-    withColumn(denominator.toString(), when(denominator.isNotNull, denominator).otherwise(0)).
-        groupBy(strata).agg((count("numerator") * var_samp(pweight * (numerator - (df.agg((sum(numerator * pweight)/sum(pweight)).alias("estimate"))
-          .select("estimate").head().getDouble(0))))/(df.agg(sum(pweight).alias("total")).head().getDouble(0))).alias("mean")).
-        agg(sum("vari").alias("variance"))
-    case (Some(cluster), Some(strata)) => df.withColumn(pweight.toString(), when(numerator.isNotNull && denominator.isNotNull, pweight).otherwise(0)).
-    withColumn(numerator.toString(), when(numerator.isNotNull, numerator).otherwise(0)).
-    withColumn(denominator.toString(), when(denominator.isNotNull, denominator).otherwise(0)).
-        groupBy(strata, cluster).agg((sum(pweight * (numerator - (denominator * df.agg((sum(numerator * pweight)/sum(denominator * pweight)).alias("ratio"))
-          .select("ratio").head().getDouble(0))))/(df.agg(sum(pweight * denominator).alias("total")).head().getDouble(0))).alias("mean")).
-        groupBy(strata).agg((count("mean") * (1 - fpc) * var_samp("mean")).alias("vari")).agg(sum("vari").alias("variance"))
-    },
-    statistic = col("ratio")
-  )
+ 
   override def svySubset(bool: Column) : TsDesign = {
     TsDesign(df.withColumn(pweight.toString(), 
                 when(bool, pweight).otherwise(0)),
                 pweight, strata, cluster, fpc)
   }
-  /*override def svyFreq(est: Column*) : SurveyStat = new SurveyStat(
-    estimate = df.groupBy(est.map(x => x): _*).agg(sum(pweight)).alias("freq"),
-    variance = df.groupBy(est.map(x => x): _*).agg(sum(pweight)).alias("freq"),
-    statistic = col("freq"),
-    variable = est
-    )*/
-  //override def svyBy(by: Column*) : GroupedTsDesign = {
-  //  GroupedTsDesign(df, by toArray, id, pweight, strata, cluster, fpc)
-  //}
 }
 
-/*case class GroupedTsDesign (
-  df: DataFrame,
-  group: Array[Column],
-  pweight: Column,
-  strata: Option[Column] = None,
-  cluster: Option[Column] = None,
-  fpc: Double = 0
-) extends GroupedSurveyDesign {
-  override def svySubset(bool: Column) : GroupedTsDesign = {
-    GroupedTsDesign(df.withColumn(pweight.toString(), 
-                when(bool, pweight).otherwise(0)),
-                group, id, pweight, strata, cluster, fpc)
-  }
-}*/
-
-sealed trait RegularReplicateDesign extends SurveyDesign {
+sealed trait ReplicateDesign extends SurveyDesign {
   def df: DataFrame
   def pweight: Column
   def repweights: Array[Column]
@@ -176,16 +53,6 @@ sealed trait RegularReplicateDesign extends SurveyDesign {
   def degf: Int = repweights.length
 }
 
-/*sealed trait GroupedReplicateDesign extends SurveyDesign {
-  def df: DataFrame
-  def group: Array[Column]
-  def pweight: Column
-  def repweights: Array[Column]
-  def mse: Boolean
-  def scale: Double
-  def rscales: Option[Array[Double]]
-}
-
 case class BrrDesign (
   df: DataFrame,
   pweight: Column,
@@ -193,14 +60,11 @@ case class BrrDesign (
   mse: Boolean = true,
   scale: Double = 1,
   rscales: Option[Array[Double]] = None
-) extends Regularreplicate_design {
-  override def svy_subset(bool: Column) : BRR_design = {
-    BRR_design(df.withColumn(pweight.toString(), 
+) extends ReplicateDesign {
+  override def svySubset(bool: Column) : BrrDesign = {
+    BrrDesign(df.withColumn(pweight.toString(), 
                 when(bool, pweight).otherwise(0)),
-                id, pweight, repweights, mse, scale, rscales)
-  }
-  override def svy_by(by: Column*) : GroupedBRR_design = {
-    GroupedBRR_design(df.groupBy(by.map(x => x): _*), id, pweight, strata, cluster, fpc)
+                pweight, repweights, mse, scale, rscales)
   }
 }
 
@@ -212,11 +76,11 @@ case class FayDesign (
   mse: Boolean = true,
   scale: Double = 1,
   rscales: Option[Array[Double]] = None
-) extends replicate_design {
-  override def svy_subset(bool: Column) : Fay_design = {
-    Fay_design(df.withColumn(pweight.toString(), 
+) extends ReplicateDesign {
+  override def svySubset(bool: Column) : FayDesign = {
+    FayDesign(df.withColumn(pweight.toString(), 
                 when(bool, pweight).otherwise(0)),
-                id, pweight, repweights, rho, mse, scale, rscales)
+                pweight, repweights, rho, mse, scale, rscales)
   }
 }
 
@@ -228,11 +92,11 @@ case class Jk1Design (
   mse: Boolean = true,
   scale: Double = 1,
   rscales: Option[Array[Double]] = None
-) extends replicate_design {
-  override def svy_subset(bool: Column) : JK1_design = {
-    JK1_design(df.withColumn(pweight.toString(), 
+) extends ReplicateDesign {
+  override def svySubset(bool: Column) : Jk1Design = {
+    Jk1Design(df.withColumn(pweight.toString(), 
                 when(bool, pweight).otherwise(0)),
-                id, pweight, repweights, fpc, mse, scale, rscales)
+                pweight, repweights, fpc, mse, scale, rscales)
   }
 }
 
@@ -244,11 +108,11 @@ case class JknDesign (
   mse: Boolean = true,
   scale: Double = 1,
   rscales: Option[Array[Double]] = None
-) extends replicate_design {
-  override def svy_subset(bool: Column) : JKn_design = {
-    JKn_design(df.withColumn(pweight.toString(), 
+) extends ReplicateDesign {
+  override def svySubset(bool: Column) : JknDesign = {
+    JknDesign(df.withColumn(pweight.toString(), 
                 when(bool, pweight).otherwise(0)),
-                id, pweight, repweights, fpc, mse, scale, rscales)
+                pweight, repweights, fpc, mse, scale, rscales)
   }
 }
 
@@ -259,11 +123,11 @@ case class BootstrapDesign (
   mse: Boolean = true,
   scale: Double = 1,
   rscales: Option[Array[Double]] = None
-) extends replicate_design {
-  override def svy_subset(bool: Column) : Bootstrap_design = {
-    Bootstrap_design(df.withColumn(pweight.toString(), 
+) extends ReplicateDesign {
+  override def svySubset(bool: Column) : BootstrapDesign = {
+    BootstrapDesign(df.withColumn(pweight.toString(), 
                 when(bool, pweight).otherwise(0)),
-                id, pweight, repweights, mse, scale, rscales)
+                pweight, repweights, mse, scale, rscales)
   }
 }
-*/
+
--- src/test/scala/com/orangutananlytics/scurvy/surveyTest.scala
+++ src/test/scala/com/orangutananlytics/scurvy/surveyTest.scala
@@ -8,22 +8,6 @@ class SurveyTest extends FunSuite {
     val spark = SparkSession.builder.master("local").appName("Simple Application").getOrCreate()
     val R = org.ddahl.rscala.RClient()
     import spark.implicits._
-    test("A SurveyStat objectis buildable") {
-
-        val test = new SurveyStat(
-            estimate = Seq(
-                ("A", 24.0)
-            ).toDF("level", "total"),
-            variance = Seq(
-                ("A", 2.5)
-            ).toDF("level", "variance"),
-            statistic = col("total")
-        )
-        assert(test.estimate.select("level").head().getString(0) === "A")
-        assert(test.variance.select("level").head().getString(0) === "A")
-        assert(test.estimate.select("total").head().getDouble(0) === 24)
-        assert(test.variance.select("variance").head().getDouble(0) === 2.5)
-    }
     
     val nhanes = spark.read.format("csv").option("header","true").option("inferSchema", "true").option("nullValue", "NA").load("data/nhanes.csv").cache()
     // R Code From Lumley
@@ -52,26 +36,26 @@ class SurveyTest extends FunSuite {
     }
     
     test("svytotal") {
-      assert(nhanes_cluster_strata.svyTotal($"HI_CHOL").estimate.select("total").head().getDouble(0).toInt === 28635245)
+      assert(SurveyTotal(nhanes_cluster_strata, $"HI_CHOL").estimate.select("total").head().getDouble(0).toInt === 28635245)
       // numerical issue so just checking we are close
-      assert(Math.abs(nhanes_cluster_strata.svyTotal($"HI_CHOL").SE.select("SE").head().getDouble(0).toInt - 2020711) <= 1)
-      assert(Math.abs(nhanes_cluster_strata.svyTotal($"HI_CHOL").cv.select("cv").head().getDouble(0) - 0.07056726) <= 0.001)
+      assert(Math.abs(SurveyTotal(nhanes_cluster_strata,$"HI_CHOL").SE.select("SE").head().getDouble(0).toInt - 2020711) <= 1)
+      assert(Math.abs(SurveyTotal(nhanes_cluster_strata,$"HI_CHOL").cv.select("cv").head().getDouble(0) - 0.07056726) <= 0.001)
       
-      assert(Math.abs(nhanes_cluster.svyTotal($"HI_CHOL").SE.select("SE").head().getDouble(0).toInt - 13767540) <= 1)
-      assert(Math.abs(nhanes_rs.svyTotal($"HI_CHOL").SE.select("SE").head().getDouble(0).toInt - 1244260) <= 1)
-      assert(Math.abs(nhanes_strata.svyTotal($"HI_CHOL").SE.select("SE").head().getDouble(0).toInt - 1240406) <= 1)
+      assert(Math.abs(SurveyTotal(nhanes_cluster,$"HI_CHOL").SE.select("SE").head().getDouble(0).toInt - 13767540) <= 1)
+      assert(Math.abs(SurveyTotal(nhanes_rs,$"HI_CHOL").SE.select("SE").head().getDouble(0).toInt - 1244260) <= 1)
+      assert(Math.abs(SurveyTotal(nhanes_strata,$"HI_CHOL").SE.select("SE").head().getDouble(0).toInt - 1240406) <= 1)
     }
     
     test("svymean") {
-      assert(Math.abs(nhanes_cluster_strata.svyMean($"HI_CHOL").estimate.select("mean").head().getDouble(0) - 0.11214) <= 0.00001)
+      assert(Math.abs(SurveyMean(nhanes_cluster_strata,$"HI_CHOL").estimate.select("mean").head().getDouble(0) - 0.11214) <= 0.00001)
       // numerical issue so just checking we are close
-      assert(Math.abs(nhanes_cluster_strata.svyMean($"HI_CHOL").variance.select("variance").head().getDouble(0) - 0.00002965717) <= 0.0000000001)
-      assert(Math.abs(nhanes_cluster_strata.svyMean($"HI_CHOL").cv.select("cv").head().getDouble(0) - 0.04856158) <= 0.00000001)
+      assert(Math.abs(SurveyMean(nhanes_cluster_strata,$"HI_CHOL").variance.select("variance").head().getDouble(0) - 0.00002965717) <= 0.0000000001)
+      assert(Math.abs(SurveyMean(nhanes_cluster_strata,$"HI_CHOL").cv.select("cv").head().getDouble(0) - 0.04856158) <= 0.00000001)
     }
     
     test("svyratio") {
-      assert(Math.abs(nhanes_cluster_strata.svySubset(col("HI_CHOL").isNotNull && col("race").isNotNull).svyRatio($"HI_CHOL", $"race").estimate.select("ratio").head().getDouble(0) - 0.05331284) <= 0.0000001)
-      assert(Math.abs(nhanes_cluster_strata.svySubset(col("HI_CHOL").isNotNull && col("race").isNotNull).svyRatio($"HI_CHOL", $"race").SE.select("SE").head().getDouble(0) - 0.002867244) <= 0.00000001)
+      assert(Math.abs(SurveyRatio(nhanes_cluster_strata,$"HI_CHOL", $"race").estimate.select("ratio").head().getDouble(0) - 0.05331284) <= 0.0000001)
+      assert(Math.abs(SurveyRatio(nhanes_cluster_strata,$"HI_CHOL", $"race").SE.select("SE").head().getDouble(0) - 0.002867244) <= 0.00000001)
     }
 
 }
